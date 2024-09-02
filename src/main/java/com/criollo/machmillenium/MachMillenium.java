cd5db380422c798d6d237cbd44de424bc0223ac3
henrriusdev <henrrybrgt@gmail.com>
2024-09-01 22:52:45 -0400

    add material-ui

--- src/main/java/com/criollo/machmillenium/MachMillenium.java
+++ src/main/java/com/criollo/machmillenium/MachMillenium.java
@@ -4,6 +4,9 @@
 
 package com.criollo.machmillenium;
 
+import com.criollo.machmillenium.vistas.Inicio;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 /**
  *
  * @author henrrius
@@ -11,6 +14,25 @@ package com.criollo.machmillenium;
 public class MachMillenium {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // Esto garantiza que toda la interfaz gráfica se ejecute en el hilo de despacho de eventos
+        SwingUtilities.invokeLater(() -> {
+            // Crear el JFrame
+            JFrame frame = new JFrame("MachMillenium");
+
+            // Establecer el comportamiento para el botón de cerrar
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+            // Crear la instancia del JPanel Inicio
+            Inicio inicio = new Inicio();
+
+            // Agregar el JPanel al JFrame
+            frame.getContentPane().add(inicio);
+
+            // Ajustar el tamaño del JFrame según el contenido
+            frame.pack();
+
+            // Hacer que la ventana sea visible
+            frame.setVisible(true);
+        });
     }
 }
