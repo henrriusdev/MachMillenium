61712075c0adfa979b04088cc552f7c4d7537a18
Henrry Bourgeot <85085805+hbourgeot@users.noreply.github.com>
2024-10-07 21:25:14 -0400

    agregando label a administrador

--- src/main/java/com/criollo/machmillenium/vistas/admin/Administrador.java
+++ src/main/java/com/criollo/machmillenium/vistas/admin/Administrador.java
@@ -1,4 +1,14 @@
 package com.criollo.machmillenium.vistas.admin;
 
+import javax.swing.*;
+
 public class Administrador {
+    public JPanel panel;
+    private JFrame jframe;
+
+    public Administrador() {}
+
+    public Administrador(JFrame jframe) {
+        this.jframe = jframe;
+    }
 }
