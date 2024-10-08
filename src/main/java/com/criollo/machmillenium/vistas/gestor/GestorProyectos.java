bbb73bd840e58637ceceb66a8e80c20c092e5ef3
Henrry Bourgeot <85085805+hbourgeot@users.noreply.github.com>
2024-10-07 21:25:26 -0400

    agregando label a GestorProyectos

--- src/main/java/com/criollo/machmillenium/vistas/gestor/GestorProyectos.java
+++ src/main/java/com/criollo/machmillenium/vistas/gestor/GestorProyectos.java
@@ -1,4 +1,14 @@
 package com.criollo.machmillenium.vistas.gestor;
 
+import javax.swing.*;
+
 public class GestorProyectos {
+    public JPanel panel;
+    private JFrame jframe;
+
+    public GestorProyectos() {}
+
+    public GestorProyectos(JFrame jframe) {
+        this.jframe = jframe;
+    }
 }
