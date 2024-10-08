d417fa2c4121e8e37ab3b539c6da964c318dea30
Henrry Bourgeot <85085805+hbourgeot@users.noreply.github.com>
2024-10-07 21:25:33 -0400

    agregando label a Usuario

--- src/main/java/com/criollo/machmillenium/vistas/usuario/Usuario.java
+++ src/main/java/com/criollo/machmillenium/vistas/usuario/Usuario.java
@@ -1,4 +1,14 @@
 package com.criollo.machmillenium.vistas.usuario;
 
+import javax.swing.*;
+
 public class Usuario {
+    public JPanel panel;
+    private JFrame jframe;
+
+    public Usuario() {}
+
+    public Usuario(JFrame jframe) {
+        this.jframe = jframe;
+    }
 }
