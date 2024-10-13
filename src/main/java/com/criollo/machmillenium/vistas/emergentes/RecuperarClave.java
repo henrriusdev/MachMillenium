b7dd975c353366d2b0fc924f25107d5a7bf87da8
Henrry Bourgeot <85085805+hbourgeot@users.noreply.github.com>
2024-10-13 17:42:17 -0400

    agregando recuperar clave

--- src/main/java/com/criollo/machmillenium/vistas/emergentes/RecuperarClave.java
+++ src/main/java/com/criollo/machmillenium/vistas/emergentes/RecuperarClave.java
@@ -1,4 +1,43 @@
 package com.criollo.machmillenium.vistas.emergentes;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class RecuperarClave {
+    private JPanel mainPanel;
+    private JLabel jLabel1;
+    private JLabel jLabel2;
+    private JButton submitButton;
+    private JPasswordField secretcode;
+    private JLabel usernameLabel;
+    private JLabel passwordLabel;
+    private JButton backButton;
+
+    public RecuperarClave() {
+        submitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (new String(secretcode.getPassword()).trim().equals("1234")) {
+                    usernameLabel.setText("admin");
+                    passwordLabel.setText("1234");
+                } else {
+                    JOptionPane.showMessageDialog(null, "Please enter correct Secret Code \n                         or\n Contact Developer for Secret Code");
+                    secretcode.setText("");
+                }
+            }
+        });
+
+        backButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFrame loginFrame = new JFrame("Login");
+                loginFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                loginFrame.pack();
+                loginFrame.setVisible(true);
+                ((JFrame) SwingUtilities.getWindowAncestor(mainPanel)).dispose(); // Cierra la ventana actual
+            }
+        });
+    }
+
 }
