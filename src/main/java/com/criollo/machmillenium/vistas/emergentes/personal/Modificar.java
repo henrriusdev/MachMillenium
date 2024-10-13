a92ac8fe99d481455ad7040d98c1b2e2630a3aeb
Henrry Bourgeot <85085805+hbourgeot@users.noreply.github.com>
2024-10-13 15:43:25 -0400

    agregando campos a personal

--- src/main/java/com/criollo/machmillenium/vistas/emergentes/personal/Modificar.java
+++ src/main/java/com/criollo/machmillenium/vistas/emergentes/personal/Modificar.java
@@ -1,4 +1,12 @@
 package com.criollo.machmillenium.vistas.emergentes.personal;
 
+import javax.swing.*;
+
 public class Modificar {
+    private JTextField textField1;
+    private JRadioButton siRadioButton;
+    private JRadioButton noRadioButton;
+    private JFormattedTextField formattedTextField1;
+    private JComboBox comboBox1;
+    private JComboBox comboBox2;
 }
