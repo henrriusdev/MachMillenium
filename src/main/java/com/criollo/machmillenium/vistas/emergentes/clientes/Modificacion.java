8657385512f30ff336e4bbcc12ae96d33050d65d
Henrry Bourgeot <85085805+hbourgeot@users.noreply.github.com>
2024-10-13 15:57:30 -0400

    agregando campos a clientes

--- src/main/java/com/criollo/machmillenium/vistas/emergentes/clientes/Modificacion.java
+++ src/main/java/com/criollo/machmillenium/vistas/emergentes/clientes/Modificacion.java
@@ -1,4 +1,10 @@
 package com.criollo.machmillenium.vistas.emergentes.clientes;
 
+import javax.swing.*;
+
 public class Modificacion {
+    private JTextField textField1;
+    private JComboBox comboBox1;
+    private JTextField textField2;
+    private JTextArea textArea1;
 }
