1454a6c63c7825efe94481565a07597f9a453726
Henrry Bourgeot <85085805+hbourgeot@users.noreply.github.com>
2024-10-16 06:28:24 -0400

    merjoando vistas

--- src/main/java/com/criollo/machmillenium/vistas/emergentes/maquinaria/AgregarMaquinaria.java
+++ src/main/java/com/criollo/machmillenium/vistas/emergentes/maquinaria/AgregarMaquinaria.java
@@ -1,10 +1,13 @@
 package com.criollo.machmillenium.vistas.emergentes.maquinaria;
 
+import com.criollo.machmillenium.entidades.Maquinaria;
+import com.criollo.machmillenium.entidades.TipoMaquinaria;
 import com.criollo.machmillenium.repos.TipoMaquinariaRepo;
 
 import javax.swing.*;
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
+import java.time.Duration;
 import java.util.Locale;
 
 public class AgregarMaquinaria {
@@ -17,17 +20,57 @@ public class AgregarMaquinaria {
     private JFormattedTextField campoCostoTotal;
     public JPanel mainPanel;
     private final TipoMaquinariaRepo tipoMaquinariaRepo;
-    
+
     public AgregarMaquinaria() {
         this.tipoMaquinariaRepo = new TipoMaquinariaRepo();
         campoTiempoEstimado.addPropertyChangeListener("value", evt -> calcularCostoTotal());
 
         // Listener para actualizar el costo total cuando cambie el costo por tiempo de uso
         campoCostoPorTiempoUso.addPropertyChangeListener("value", evt -> calcularCostoTotal());
+        cerrarButton.addActionListener(e -> {
+            // Cerrar la ventana emergente
+            JFrame frame = (JFrame) SwingUtilities.getWindowAncestor(mainPanel);
+            frame.dispose();
+        });
+        agregarButton.addActionListener(e -> {
+            // Insertar la maquinaria en la base de datos
+            try {
+                // Crear un objeto de ModeloMaquinaria con los valores de los campos
+                Maquinaria maquinaria = new Maquinaria();
+                maquinaria.setNombre(campoNombre.getText());
+
+                TipoMaquinaria tipoMaquinaria = tipoMaquinariaRepo.obtenerPorNombre((String) comboboxTipoMaquinaria.getSelectedItem());
+                maquinaria.setTipoMaquinaria(tipoMaquinaria);
+
+                Duration tiempoEstimado = Duration.ofHours(((Number) campoTiempoEstimado.getValue()).longValue());
+                maquinaria.setTiempoEstimadoDeUso(tiempoEstimado);
+
+                // Obtener el valor de campoCostoPorTiempoUso como Double
+                maquinaria.setCostoPorTiempoDeUso(((Number) campoCostoPorTiempoUso.getValue()).doubleValue());
+
+                // Obtener el valor de campoCostoTotal como Double
+                maquinaria.setCostoTotal(((Number) campoCostoTotal.getValue()).doubleValue());
+
+                // Insertar la maquinaria en la base de datos
+                tipoMaquinariaRepo.insertarMaquinaria(maquinaria);
+
+                // Mostrar un mensaje de éxito
+                JOptionPane.showMessageDialog(mainPanel, "Maquinaria insertada correctamente");
+                // Cerrar la ventana emergente
+                JFrame frame = (JFrame) SwingUtilities.getWindowAncestor(mainPanel);
+                frame.dispose();
+            } catch (Exception ex) {
+                JOptionPane.showMessageDialog(mainPanel, "Error al insertar la maquinaria: " + ex.getMessage());
+                ex.printStackTrace();
+            }
+        });
     }
 
     private void calcularCostoTotal() {
         try {
+            if (campoTiempoEstimado.getValue() == null || campoCostoPorTiempoUso.getValue() == null) {
+                return;
+            }
             // Obteniendo los valores de los campos
             double tiempoEstimado = ((Number) campoTiempoEstimado.getValue()).doubleValue();
             double costoPorTiempo = ((Number) campoCostoPorTiempoUso.getValue()).doubleValue();
@@ -43,6 +86,7 @@ public class AgregarMaquinaria {
     }
 
     private void createUIComponents() {
+        TipoMaquinariaRepo tipoMaquinariaRepo = new TipoMaquinariaRepo();
         // Configurar el campo de tiempo estimado (horas y minutos)
         NumberFormat formatoTiempo = NumberFormat.getNumberInstance();
         campoTiempoEstimado = new JFormattedTextField(formatoTiempo);
--- src/main/java/com/criollo/machmillenium/vistas/emergentes/maquinaria/ModificarMaquinaria.java
+++ src/main/java/com/criollo/machmillenium/vistas/emergentes/maquinaria/ModificarMaquinaria.java
@@ -1,26 +1,89 @@
 package com.criollo.machmillenium.vistas.emergentes.maquinaria;
 
+import com.criollo.machmillenium.entidades.Maquinaria;
+import com.criollo.machmillenium.entidades.TipoMaquinaria;
+import com.criollo.machmillenium.modelos.ModeloMaquinaria;
 import com.criollo.machmillenium.repos.TipoMaquinariaRepo;
 
 import javax.swing.*;
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
+import java.time.Duration;
 import java.util.Locale;
 
 public class ModificarMaquinaria {
 
-    private JPanel mainPanel;
+    public JPanel mainPanel;
     private JTextField campoNombre;
     private JComboBox<String> comboboxTipoMaquinaria;
     private JFormattedTextField campoCostoPorTiempoUso;
     private JFormattedTextField campoTiempoEstimado;
     private JButton cerrarButton;
-    private JButton agregarButton;
+    private JButton editarButton;
     private JFormattedTextField campoCostoTotal;
     private final TipoMaquinariaRepo tipoMaquinariaRepo;
+    private final ModeloMaquinaria modeloMaquinaria;
 
-    public ModificarMaquinaria() {
+    public ModificarMaquinaria(ModeloMaquinaria modeloMaquinaria) {
+        if (modeloMaquinaria == null) {
+            JOptionPane.showMessageDialog(mainPanel, "No se puede modificar una maquinaria nula");
+            this.modeloMaquinaria = null;
+            this.tipoMaquinariaRepo = null;
+            return;
+        }
+
+        this.modeloMaquinaria = modeloMaquinaria;
         this.tipoMaquinariaRepo = new TipoMaquinariaRepo();
+
+        // Configurar los campos con los valores de la maquinaria
+        campoNombre.setText(modeloMaquinaria.getNombre());
+
+        TipoMaquinaria tipoMaquinaria = tipoMaquinariaRepo.obtenerPorId(modeloMaquinaria.getTipoMaquinariaId());
+        comboboxTipoMaquinaria.setSelectedItem(tipoMaquinaria.getNombre());
+
+        long horas = Long.parseLong(modeloMaquinaria.getTiempoEstimadoDeUso().split(" ")[0]);
+        long minutos = Long.parseLong(modeloMaquinaria.getTiempoEstimadoDeUso().split(" ")[3]);
+        campoTiempoEstimado.setValue(horas + minutos / 60);
+
+        campoCostoPorTiempoUso.setValue(modeloMaquinaria.getCostoPorTiempoDeUso());
+        campoCostoTotal.setValue(modeloMaquinaria.getCostoTotal());
+
+        cerrarButton.addActionListener(e -> {
+            // Cerrar la ventana emergente
+            JFrame frame = (JFrame) SwingUtilities.getWindowAncestor(mainPanel);
+            frame.dispose();
+        });
+
+        editarButton.addActionListener(e -> {
+            // Actualizar la maquinaria en la base de datos
+            try {
+                // Crear un objeto de Maquinaria con los valores de los campos
+                Maquinaria maquinaria = new Maquinaria();
+                maquinaria.setId(modeloMaquinaria.getId());
+                maquinaria.setNombre(campoNombre.getText());
+
+                TipoMaquinaria tipoMaquinariaNuevo = tipoMaquinariaRepo.obtenerPorNombre((String) comboboxTipoMaquinaria.getSelectedItem());
+                maquinaria.setTipoMaquinaria(tipoMaquinariaNuevo);
+
+                double tiempoEstimado = ((Number) campoTiempoEstimado.getValue()).doubleValue();
+                maquinaria.setTiempoEstimadoDeUso(Duration.ofHours((long) tiempoEstimado));
+
+                maquinaria.setCostoPorTiempoDeUso(((Number) campoCostoPorTiempoUso.getValue()).doubleValue());
+                maquinaria.setCostoTotal(((Number) campoCostoTotal.getValue()).doubleValue());
+
+                // Actualizar la maquinaria en la base de datos
+                tipoMaquinariaRepo.actualizarMaquinaria(maquinaria);
+
+                // Mostrar un mensaje de éxito
+                JOptionPane.showMessageDialog(mainPanel, "Maquinaria actualizada correctamente");
+                // Cerrar la ventana emergente
+                JFrame frame = (JFrame) SwingUtilities.getWindowAncestor(mainPanel);
+                frame.dispose();
+            } catch (Exception ex) {
+                JOptionPane.showMessageDialog(mainPanel, "Error al actualizar la maquinaria: " + ex.getMessage());
+            }
+        });
+
         campoTiempoEstimado.addPropertyChangeListener("value", evt -> calcularCostoTotal());
 
         // Listener para actualizar el costo total cuando cambie el costo por tiempo de uso
@@ -44,6 +107,7 @@ public class ModificarMaquinaria {
     }
 
     private void createUIComponents() {
+        TipoMaquinariaRepo tipoMaquinariaRepo = new TipoMaquinariaRepo();
         // Configurar el campo de tiempo estimado (horas y minutos)
         NumberFormat formatoTiempo = NumberFormat.getNumberInstance();
         campoTiempoEstimado = new JFormattedTextField(formatoTiempo);
